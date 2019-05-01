22eb063275c852dc9c43c30da4c02e618d867fc8
AliNisarAhmed <ali.nisar87@gmail.com>
2019-05-01 11:10:35 -0600

    generating dynamic slugs from .md posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,55 @@
-const path = require('path');
+const path = require("path");
 
 // onCreateNode generates a slug based on the markdown fiels in our .md post files
 
 module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions;
+    const { createNodeField } = actions;
 
-  if (node.internal.type === 'MarkdownRemark') {    // console.log(node) to see that all .md files have type 'MarkdownRemark'
-    const slug = path.basename(node.fileAbsolutePath, '.md');  // from '/path/to/the/filename.md' to 'filename'
-    createNodeField({   
-      // because of this function call, 
-      // in each "node", that is, each .md file in the gatsby playground, a new prop "fiels" has been added
-      // which contain the "slug" value being passed here
-      node,
-      name: 'slug',
-      value: slug
-    })
-  }
+    if (node.internal.type === "MarkdownRemark") {
+        // console.log(node) to see that all .md files have type 'MarkdownRemark'
+        const slug = path.basename(node.fileAbsolutePath, ".md"); // from '/path/to/the/filename.md' to 'filename'
+        createNodeField({
+            // because of this function call,
+            // in each "node", that is, each .md file in the gatsby playground, a new prop "fiels" has been added
+            // which contain the "slug" value being passed here
+            node,
+            name: "slug",
+            value: slug,
+        });
+    }
+};
 
-  // console.log(JSON.stringify(actions, undefined, 4));
-}
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    // 1. Get path to template
+    // 2. Get markdown data
+    // 3. Create new pages
+
+    const blogTemplate = path.resolve("./src/templates/blog.js");
+
+    const response = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    response.data.allMarkdownRemark.edges.forEach(edge => {
+        console.log(edge);
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug,
+            },
+        });
+    });
+};
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import Layout from '../components/layout';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react";
+import Layout from "../components/layout";
+import { useStaticQuery, graphql, Link } from "gatsby";
 
 export default () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              title
-              date
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                        fields {
+                            slug
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  `);
+    `);
 
-  return (
-    <Layout>
-      <h1>This is the blog page</h1>
-      <hr />
-      <ol>
-        {
-          data.allMarkdownRemark.edges.map(edge => (
-            <li>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
-              <hr />
-            </li>
-          ))
-        }
-      </ol>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <h1>This is the blog page</h1>
+            <hr />
+            <ol>
+                {data.allMarkdownRemark.edges.map(edge => (
+                    <li>
+                        <Link to={`/blog/${edge.node.fields.slug}`}>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </Link>
+                        <hr />
+                    </li>
+                ))}
+            </ol>
+        </Layout>
+    );
 };
